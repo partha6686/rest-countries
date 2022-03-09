d4be61726011ccc6b65bf7aea94f747487c9e84f
Partha Sarathi Praharaj <parthasarathi6686@gmail.com>
2022-03-09 20:30:28 +0530

    Pre Deploy

--- src/components/FilterBar.js
+++ src/components/FilterBar.js
@@ -1,49 +1,78 @@
-import React, { useState, useEffect } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { Link, Redirect } from "react-router-dom";
 
-function FilterBar(props){
-    const [query,setQuery] = useState('');
-    const [region,setRegion] = useState('');
-    
-    const handleChange = (e)=>{
-        setQuery(e.target.value);
+function FilterBar(props) {
+  const [query, setQuery] = useState("");
+  const [region, setRegion] = useState("");
+
+  const handleChange = (e) => {
+    setQuery(e.target.value);
+  };
+  const handleSubmit = () => {
+    props.setFinal(query);
+  };
+  const handleRegion = (e) => {
+    setRegion(e.target.value);
+  };
+  const changeRegion = () => {
+    props.setFinalReg(region);
+  };
+  useEffect(() => {
+    if (region) {
+      changeRegion();
     }
-    const handleSubmit = ()=>{
-        props.setFinal(query);
-    }
-    const handleRegion = (e)=>{
-        setRegion(e.target.value);
-    }
-    const changeRegion = ()=>{
-        props.setFinalReg(region);
-    }
-    useEffect(() => {
-        if(region){
-            changeRegion();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [region]);
+  return (
+    <div
+      className={
+        props.theme === "light" ? "filter-bar" : "filter-bar filter-bar-dark"
+      }
+    >
+      <div
+        className={
+          props.theme === "light"
+            ? "custom-search-bar"
+            : "custom-search-bar custom-search-bar-dark"
         }
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [region])
-    return(
-        <div className={props.theme==='light'?'filter-bar':'filter-bar filter-bar-dark'}>
-            <div className={props.theme==='light'?'custom-search-bar':'custom-search-bar custom-search-bar-dark' }>
-                <Link to={{
-                    pathname: "/query",
-                    search: "?q=" + query
-                }}><button onClick={handleSubmit}><FontAwesomeIcon icon={faSearch} /></button></Link>
-                <input onChange={handleChange} type='text' placeholder='Search for a country' name='q' value={query}/>
-            </div>
-            <select onChange={handleRegion} name="region" id="region" defaultValue='DEFAULT'>
-                <option value='DEFAULT' disabled hidden>Filter by Region</option>
-                <option value="africa">Africa</option>
-                <option value="america">America</option>
-                <option value="asia">Asia</option>
-                <option value="europe">Europe</option>
-                <option value="oceania">Oceania</option>
-            </select>
-        </div>
-    );
+      >
+        <Link
+          to={{
+            pathname: "/query",
+            search: "?q=" + query,
+          }}
+        >
+          <button onClick={handleSubmit}>
+            <FontAwesomeIcon icon={faSearch} />
+          </button>
+        </Link>
+        <input
+          onChange={handleChange}
+          type="text"
+          placeholder="Search for a country"
+          name="q"
+          value={query}
+        />
+      </div>
+      <select
+        onChange={handleRegion}
+        name="region"
+        id="region"
+        defaultValue="DEFAULT"
+      >
+        <option value="DEFAULT" disabled hidden>
+          Filter by Region
+        </option>
+        <option value="africa">Africa</option>
+        <option value="america">America</option>
+        <option value="asia">Asia</option>
+        <option value="europe">Europe</option>
+        <option value="oceania">Oceania</option>
+      </select>
+    </div>
+  );
 }
 
 export default FilterBar;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,20 +1,26 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMoon } from '@fortawesome/free-solid-svg-icons'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMoon } from "@fortawesome/free-solid-svg-icons";
 
-function NavBar(props){
-    const handleClick = ()=>{
-        if(props.theme === 'light' ){
-            props.setTheme('dark');
-        }else{
-            props.setTheme('light');
-        }
+function NavBar(props) {
+  const handleClick = () => {
+    if (props.theme === "light") {
+      props.setTheme("dark");
+    } else {
+      props.setTheme("light");
     }
-    return(
-        <div className={props.theme==='light'?'nav-bar':'nav-bar nav-bar-dark'}>
-            <a href='/'><h1>Where in the world?</h1></a>  
-            <p onClick={handleClick}><FontAwesomeIcon icon={faMoon} /> Dark Mode</p>
-        </div>
-    )
+  };
+  return (
+    <div
+      className={props.theme === "light" ? "nav-bar" : "nav-bar nav-bar-dark"}
+    >
+      <a href="/">
+        <h1>Where in the world?</h1>
+      </a>
+      <p onClick={handleClick}>
+        <FontAwesomeIcon icon={faMoon} /> Dark Mode
+      </p>
+    </div>
+  );
 }
 export default NavBar;
